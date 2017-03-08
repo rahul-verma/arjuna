cded4a2c294e41b190472f31f53fb838713e71a2
Rahul Verma <rv@autocognite.com>
2017-03-08 22:42:36 +0530

    0.1 WIP

--- AC-Batteries/src/com/autocognite/pvt/batteries/value/StringValue.java
+++ AC-Batteries/src/com/autocognite/pvt/batteries/value/StringValue.java
@@ -1,11 +1,17 @@
 package com.autocognite.pvt.batteries.value;
 
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import com.autocognite.arjuna.enums.ValueType;
 import com.autocognite.arjuna.interfaces.Value;
 
 public class StringValue extends AbstractValue {
+	private static Set<String> trues = new HashSet<String>(Arrays.asList("YES", "TRUE", "ON", "1"));
+	private static Set<String> falses = new HashSet<String>(Arrays.asList("NO", "FALSE", "OFF", "0"));
 
 	public StringValue(String strObject) {
 		super(ValueType.STRING, strObject);
@@ -37,5 +43,114 @@ public class StringValue extends AbstractValue {
 			throw new UnsupportedRepresentationException(StringValue.class.getSimpleName(), "asEnum()", this.toString(),
 					enumClass.getSimpleName());
 		}
+	}
+	
+	@Override
+	public boolean asBoolean() throws Exception {
+		String uStr = this.asString().toUpperCase().trim();
+		if (trues.contains(uStr)){
+			return true;
+		} else if (falses.contains(uStr)){
+			return false;
+		}
+		throw new Exception(String.format(">>%s<< can not be represented as a boolean.", this.asString()));
+	}
+
+	@Override
+	public Number asNumber() throws Exception {
+		try{
+			if (this.asString().matches("(\\-)?[0-9\\.]+")){
+				try{
+					return this.asFloat();
+				} catch (Exception e){
+					return this.asDouble();
+				}
+			} else if (this.asString().matches("(\\-)?[0-9]+")){
+				try{
+					return this.asInt();
+				} catch (Exception e){
+					return this.asLong();
+				}				
+			}
+		} catch (Exception e){
+			// do nothing. The subsequent exception is alright.
+		}
+		throw new Exception(String.format("Not supported for %s", this.getClass().getSimpleName()));
+	}
+
+	@Override
+	public int asInt() throws Exception {
+		try{
+			return Integer.valueOf(this.asString());
+		} catch (Exception e){
+			throw new Exception(String.format(">>%s<< can not be represented as an integer.", this.asString()));
+		}
+	}
+
+	@Override
+	public long asLong() throws Exception {
+		try{
+			return Long.valueOf(this.asString());
+		} catch (Exception e){
+			throw new Exception(String.format(">>%s<< can not be represented as a long int.", this.asString()));
+		}
+	}
+
+	@Override
+	public double asDouble() throws Exception {
+		try{
+			return Double.valueOf(this.asString());
+		} catch (Exception e){
+			throw new Exception(String.format(">>%s<< can not be represented as a double.", this.asString()));
+		}
+	}
+
+	@Override
+	public float asFloat() throws Exception {
+		try{
+			return Float.valueOf(this.asString());
+		} catch (Exception e){
+			throw new Exception(String.format(">>%s<< can not be represented as a float.", this.asString()));
+		}
+	}
+
+	@Override
+	public <T extends Enum<T>> List<T> asEnumList(Class<T> klass) throws Exception {
+		try{
+			return Arrays.asList(this.asEnum(klass));
+		} catch (Exception e){
+			throw new Exception(String.format(">>%s<< can not be represented as a list of enum of type %s.", this.asString(), klass.getName()));
+		}
+	}
+
+	@Override
+	public List<Number> asNumberList() throws Exception {
+		try{
+			return Arrays.asList(this.asNumber());
+		} catch (Exception e){
+			throw new Exception(String.format(">>%s<< can not be represented as a list of numbers.", this.asString()));
+		}
+	}
+
+	@Override
+	public List<Integer> asIntList() throws Exception {
+		try{
+			return Arrays.asList(this.asInt());
+		} catch (Exception e){
+			throw new Exception(String.format(">>%s<< can not be represented as a list of integers.", this.asString()));
+		}
+	}
+
+	@Override
+	public List<String> asStringList() throws Exception {
+		try{
+			return Arrays.asList(this.asString());
+		} catch (Exception e){
+			throw new Exception(String.format(">>%s<< can not be represented as a list of strings.", this.asString()));
+		}
+	}
+
+	public List<?> asList() throws Exception {
+		return this.asStringList();
 	}
 }
