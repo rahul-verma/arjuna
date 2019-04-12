b14440b7b8d18a02c825b2eb89888a72e9f84b75
Rahul Verma <rahulverma81@gmail.com>
2019-04-12 13:55:41 +0530

    Deleted extraneous example file

--- basicex.py
+++
@@ -1,9 +0,0 @@
-from arjuna.tpi import Arjuna
-
-config = Arjuna.init()
-
-automator = Arjuna.createGuiAutomator(config)
-
-automator.browser().goToUrl("https://www.google.com")
-print(automator.mainWindow().getTitle())
-automator.quit()
