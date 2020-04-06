2664007b28703a061dcb20eb935d7563d1d981c4
Rahul Verma <rahulverma81@gmail.com>
2020-04-06 15:21:07 +0530

    sphinx conf update

--- docs/sdoc/source/conf.py
+++ docs/sdoc/source/conf.py
@@ -12,6 +12,10 @@
 #
 import os
 import sys
+
+def __join_paths(*paths):
+    return os.path.abspath(os.path.join(*paths))
+
 __root_dir = __join_paths(os.path.dirname(os.path.realpath(__file__)), "..", "..", "..")
 print(__root_dir)
 sys.path.insert(0, __root_dir)
