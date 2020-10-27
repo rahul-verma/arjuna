93590fc4bdce691cd61223d14080c0dd025be5dc
Rahul Verma <rahulverma81@gmail.com>
2020-10-27 19:36:30 +0530

    1.1.19 base checkin

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = "arjuna",
-    version = "1.1.18",
+    version = "1.1.19",
     url = "https://rahulverma.net",
     description = "Arjuna is a Python based test automation framework developed by Rahul Verma (www.rahulverma.net).",
     author = "Rahul Verma",
