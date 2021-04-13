07c729e22b5b52717ff484c556e43b78c47bd4c4
Rahul Verma <rahulverma81@gmail.com>
2021-04-13 11:43:43 +0530

    1.1.42 base checkin

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ copyright = '2015-2021, Rahul Verma'
 author = 'Rahul Verma'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.41'
+release = '1.1.42'
 
 
 # -- General configuration ---------------------------------------------------
